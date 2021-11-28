52450efe57558eac09ce5300ceb0f90eb602d1a3
CommieTaco <rolin.azmitia.iii@gmail.com>
2021-11-27 22:44:49 -0600

    Added React-Redux to the app

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import store from './app/store';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
