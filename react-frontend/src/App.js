75dd77e43f3d592f039fbfe4b0abd18fd5b5144e
CommieTaco <rolin.azmitia.iii@gmail.com>
2021-11-29 14:04:40 -0600

    Connected to the API

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,27 +1,11 @@
-import './App.scss';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Users from './components/Users';
-import Landing from './components/Landing';
+import "./App.scss";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Routes>
-          <Route path='/' element={(
-            <>
-              <Landing/>
-            </>
-          )} />
-
-          <Route path='/users' element={(
-            <Users/>
-          )}/>
-        </Routes>
-        
-      </div>
-    </Router>
-    
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
--- react-frontend/src/components/Landing.js
+++ react-frontend/src/components/Landing.js
@@ -1,11 +1,9 @@
 const Landing = () => {
-    return (
-        <div className='container'>
-            <h1>
-                Welcome to Yeeting
-            </h1>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <h1>Welcome to HappyPaws | Alpha 1.0.0</h1>
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
